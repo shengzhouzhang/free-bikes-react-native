0c9fba83db391df6096b6079f655e7b0efa3e109
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-03 13:59:38 +1100

    add parseRawToEntities function

--- specs/utils/parser.js
+++ specs/utils/parser.js
@@ -1,12 +1,11 @@
 
 import raw from '../../data/bikes.json';
-import { parseRawToEntity } from '../../utils/parser';
+import { parseRawToEntities } from '../../utils/parser';
 
 describe('parser util', () => {
 
-  console.log(raw);
-
   it('should parse raw data into entity', () => {
-
+    let entities = parseRawToEntities(raw);
+    console.log(entities);
   });
 });
--- utils/parser.js
+++ utils/parser.js
@@ -1,4 +1,16 @@
 
-export function parseRawToEntity (raw = {}) {
+import _ from 'lodash';
 
+export function parseRawToEntities (raw = {}) {
+  return _.map(raw.data, item => {
+    return {
+      id: item[8],
+      name: item[9],
+      numberOfBikes: item[11],
+      position: {
+        lat: item[14][1],
+        lng: item[14][2]
+      }
+    };
+  });
 };
