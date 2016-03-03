5ee3651f62d62133a8a99d7b75c300641e522a30
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-03 16:36:33 +1100

    add show results

--- components/SearchTool/index.js
+++ components/SearchTool/index.js
@@ -4,24 +4,37 @@ import React from 'react-native';
 import styles from '../../style';
 import Input from '../../components/SearchTool/Input';
 import Results from '../../components/SearchTool/Result';
+import SelectedStation from '../../stores/station';
 
 export default class SearchTool extends React.Component {
   static propTypes = {
     stations: React.PropTypes.array.isRequired
   };
   state = {
-    text: 'south'
+    text: 'south',
+    showResults: true
   };
   render = () => {
     return (
       <React.View style={styles.searchTool}>
         <Input value={this.state.text} onTextChange={this.onTextChangeHandler} />
-        <Results stations={this.filter(this.props.stations, this.state.text)} />
+        { this.state.showResults ? (<Results stations={this.filter(this.props.stations, this.state.text)} />) : (undefined) }
       </React.View>
     );
   };
+  componentDidMount = () => {
+    this.unsubscribe = SelectedStation.subscribe(() => {
+      this.onSelectStationHandler();
+    });
+  };
+  componentWillUnmount = () => {
+    this.unsubscribe();
+  };
+  onSelectStationHandler = () => {
+    this.setState({ showResults: false });
+  };
   onTextChangeHandler = (text) => {
-    this.setState({ text: text });
+    this.setState({ text: text, showResults: true });
   };
   filter = (entities, keyword = '') => {
     if (keyword.length < 3) { return []; }
