02637008cdf8d36a2d62a141c0bbc839f60d706d
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-04 19:56:16 +1100

    add test for Direction button

---
+++ specs/components/Map/Direction.js
@@ -0,0 +1,43 @@
+
+import React from 'react-native';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import Direction from '../../../components/Map/Direction';
+
+describe('Direction Component', () => {
+
+  const TEST_ADDRESS = 'a test address';
+
+  describe('render Text', () => {
+
+    it('should render Text with Get Direction text', () => {
+      let wrapper = shallow(<Direction address={TEST_ADDRESS} />);
+      expect(wrapper.contains(<React.Text>Get Direction</React.Text>)).to.eql(true);
+    });
+  });
+
+  describe('render TouchableHighlight', () => {
+
+    it('should provide onPress handler', () => {
+      let wrapper = shallow(<Direction address={TEST_ADDRESS} />);
+      expect(wrapper.find(React.TouchableHighlight).prop('onPress')).to.eql(wrapper.instance().onPressHandler);
+    });
+  });
+
+  describe('onPressHandler', () => {
+
+    it('should invalve the call of prop onTextChange', () => {
+      let wrapper = shallow(<Direction address={TEST_ADDRESS} />);
+      expect(wrapper.instance().onPressHandler).to.throws('LinkingManager is not defined');
+    });
+  });
+
+  describe('encodeUrl', () => {
+
+    it('should replace blank speces with +', () => {
+      let wrapper = shallow(<Direction address={TEST_ADDRESS} />);
+      expect(wrapper.instance().encodeUrl(TEST_ADDRESS)).to.eql(TEST_ADDRESS.split(' ').join('+'));
+    });
+  });
+});
