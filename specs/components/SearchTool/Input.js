4b43859c94f8a7ad3114dfbda6043cffd2c222fe
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-04 19:34:10 +1100

    add test for input component

---
+++ specs/components/SearchTool/Input.js
@@ -0,0 +1,33 @@
+
+import React from 'react-native';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import Input from '../../../components/SearchTool/Input';
+
+describe('Input Component', () => {
+
+  const TEST_VALUE = 'keyword';
+
+  describe('render TextInput', () => {
+
+    it('should render TextInput and provide the value and onChange handler', () => {
+      let onTextChangeHandler = sinon.spy();
+      let wrapper = shallow(<Input value={TEST_VALUE} onTextChange={onTextChangeHandler} />);
+      expect(wrapper.find(React.TextInput)).to.have.length(1);
+      expect(wrapper.find(React.TextInput).prop('value')).to.eql(TEST_VALUE);
+      expect(wrapper.find(React.TextInput).prop('onChange')).to.eql(wrapper.instance().onTextChangeHandler);
+    });
+  });
+
+  describe('onTextChangeHandler', () => {
+
+    it('should invalve the call of prop onTextChange', () => {
+      let onTextChangeHandler = sinon.spy();
+      let wrapper = shallow(<Input value="" onTextChange={onTextChangeHandler} />);
+      wrapper.instance().onTextChangeHandler({ nativeEvent: { text: TEST_VALUE }});
+      expect(onTextChangeHandler.called).eql(true);
+      expect(onTextChangeHandler.getCall(0).args[0]).eql(TEST_VALUE);
+    });
+  });
+});
